b17f501e6863eea066a3a249143b88cdab392cae
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-19 01:14:30 +0500

    Update task_02.cpp

---
+++ Assignment_02/task_02.cpp
@@ -0,0 +1,19 @@
+//Design a class Line that implements a line, which is represented by the formula y = ax + b.Your class should store a and b as double member variables.Write a member function intersect(ℓ) that returns the x coordinate at which this line intersects line ℓ. If the two lines are parallel, then your function should throw an exception Parallel. Write a C++ program that creates a number of Line objects and tests each pair for intersection. Your program should print an appropriate error message for parallel lines
+#include <iostream>
+#include <cmath>
+class Line
+{
+    public:
+    double a,b;
+    Line(double a,double b)
+    {
+        this->a=a;
+        this->b=b;
+    }
+    double intersect(l)
+    {
+        
+        
+    }
+
+};
