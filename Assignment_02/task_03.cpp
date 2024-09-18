40b31172ff55e43d761a6dee1a0890396a39a03a
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-19 01:37:07 +0500

    Create task_03.cpp

---
+++ Assignment_02/task_03.cpp
@@ -0,0 +1,7 @@
+//Write a C++ class that is derived from the Progression class to produce a progression where each value is the absolute value of the difference between the previous two values.You should include a default constructor that starts with 2 and 200 as the first two values and a parametric constructor that starts with a specified pair of numbers as the first two values.
+//progression1.cpp
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "progression.h"
+#include "progression1.h"
