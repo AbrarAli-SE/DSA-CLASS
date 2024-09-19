4d62a37fe5f387440a998826045a84c0025bf019
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-19 23:11:52 +0500

    Update task_03.cpp

--- Assignment_02/task_03.cpp
+++ Assignment_02/task_03.cpp
@@ -1,4 +1,4 @@
-//Write a C++ class that is derived from the Progression class to produce a progression where each value is the absolute value of the difference between the previous two values.You should include a default constructor that starts with 2 and 200 as the first two values and a parametric constructor that starts with a specified pair of numbers as the first two values.
+// Write a C++ class that is derived from the Progression class to produce a progression where each value is the absolute value of the difference between the previous two values.You should include a default constructor that starts with 2 and 200 as the first two values and a parametric constructor that starts with a specified pair of numbers as the first two values.
 #include <iostream>
 using namespace std;
 
@@ -8,13 +8,14 @@ protected:
     int current;
 
 public:
-    Progression(int start = 0) : current(start) {}
-
+    Progression(int start = 0)
+    {
+        current = start;
+    }
     virtual int nextValue()
     {
         return ++current;
     }
-
     void printProgression(int n)
     {
         cout << current;
@@ -33,13 +34,28 @@ private:
 
     int abs(int x)
     {
-        return (x < 0) ? -x : x;
+        if (x < 0)
+        {
+            return -x;
+        }
+        else
+        {
+            return x;
+        }
     }
 
 public:
-    AbsDiffProgression() : Progression(200), prev(2) {}
+    AbsDiffProgression()
+    {
+        current = 200;
+        prev = 2;
+    }
 
-    AbsDiffProgression(int first, int second) : Progression(second), prev(first) {}
+    AbsDiffProgression(int first, int second)
+    {
+        current = second;
+        prev = first;
+    }
 
     virtual int nextValue() override
     {
@@ -54,18 +70,17 @@ int main()
 {
     int numTerms;
 
-    // Ask user for the number of terms
-    cout << "Enter the number of terms you want to print: ";
+    cout << "\n\tEnter the number of terms you want to print: ";
     cin >> numTerms;
 
-    cout << "Default Progression (starting with 2 and 200):" << endl;
+    cout << "\n\tDefault Progression (starting with 2 and 200): " << endl;
     AbsDiffProgression defaultProg;
     defaultProg.printProgression(numTerms);
 
     int first, second;
-    cout << "Enter the first value for custom progression: ";
+    cout << "\n\tEnter the first value for custom progression: ";
     cin >> first;
-    cout << "Enter the second value for custom progression: ";
+    cout << "\tEnter the second value for custom progression: ";
     cin >> second;
 
     AbsDiffProgression customProg(first, second);
